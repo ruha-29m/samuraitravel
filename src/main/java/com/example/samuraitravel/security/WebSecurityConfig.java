a38c59bc2355efadbda70bddeb66ad940025e3af
ruha-29m <haruka.a.kick@gmail.com>
2025-08-29 00:21:47 +0900

    87章まで作成

--- src/main/java/com/example/samuraitravel/security/WebSecurityConfig.java
+++ src/main/java/com/example/samuraitravel/security/WebSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -16,22 +17,22 @@ public class WebSecurityConfig {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
-				.authorizeHttpRequests(requests -> requests
+				.authorizeHttpRequests((requests) -> requests
 						.requestMatchers("/css/**", "/images/**", "/js/**", "/storage/**", "/", "/signup/**", "/houses",
-								"/houses/{id}")
+								"/houses/{id}", "/stripe/webhook")
 						.permitAll() // すべてのユーザーにアクセスを許可するURL
-						.requestMatchers("/admin/**").hasRole("ADMIN") // 管理者にのみアクセスを許可するURL
 						.anyRequest().authenticated() // 上記以外のURLはログインが必要（会員または管理者のどちらでもOK）
 				)
-				.formLogin(form -> form
+				.formLogin((form) -> form
 						.loginPage("/login") // ログインページのURL
 						.loginProcessingUrl("/login") // ログインフォームの送信先URL
 						.defaultSuccessUrl("/?loggedIn") // ログイン成功時のリダイレクト先URL
 						.failureUrl("/login?error") // ログイン失敗時のリダイレクト先URL
 						.permitAll())
-				.logout(logout -> logout
+				.logout((logout) -> logout
 						.logoutSuccessUrl("/?loggedOut") // ログアウト時のリダイレクト先URL
-						.permitAll());
+						.permitAll())
+				.csrf(csrf -> csrf.ignoringRequestMatchers(new AntPathRequestMatcher("/stripe/webhook")));
 
 		return http.build();
 	}
